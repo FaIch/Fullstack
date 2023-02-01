9667467b03ec668fc0c9fa4a18c0730c5a525f35
Joakim <joakimfalch@outlook.com>
2023-02-01 15:44:26 +0100

    Started working on øving 3

---
+++ oving_3/src/components/History.vue
@@ -0,0 +1,35 @@
+<template>
+    <div id="history-box">
+        <ul>
+            <li v-for="(equation, index) in equation_list" :key="index">
+                {{ equation }}
+            </li>
+        </ul>
+    </div>    
+</template>
+
+<script>
+export default{
+    name: "History",
+    props: [
+        "equation_list"
+    ]
+}
+</script>
+
+<style scoped>
+    #history-box{
+        height: 580px;
+        width: 200px;
+        background-color: white;
+        border-style: solid;
+        border-color: black;
+        border-width: 2px;
+        overflow: auto;
+    }
+
+    li{
+        list-style-type: none;
+        font-size: 18px;
+    }
+</style>
---
+++ oving_3/src/views/CalculatorView.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="container">
+    <div class="calc-div"><Calculator @addEquation="getEquation"/></div>
+    <div><History :equation_list="all_equations"/></div>
+  </div>
+</template>
+
+<script>
+import Calculator from '@/components/Calculator.vue'
+import History from '@/components/History.vue';
+
+export default {
+  name: 'CalculatorView',
+  components: {
+    Calculator,
+    History
+  },
+
+  data: () => {
+    return {
+      all_equations: []
+    }
+  },
+  methods: {
+    getEquation(sent_equation){
+      this.all_equations.push(sent_equation);
+    }
+  }
+}
+</script>
+<style>
+html{
+    box-sizing: border-box;
+    height: 100%;
+}
+
+body{
+    align-items: center;
+    background: linear-gradient(320deg, #2D3142, #4F5D75, #63c9b4);
+    display: flex;
+    height: inherit;
+    justify-content: center;
+}
+
+.container{
+  display: flex;
+}
+
+.calc-div{
+  padding-right: 40px;
+}
+</style>
+
